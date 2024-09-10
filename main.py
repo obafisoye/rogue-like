0749c798bf8e3f78dc57bb8c476aabe8ce3d275b
obafisoye <dannieoba@gmail.com>
2024-09-10 12:01:24 -0500

    Added Player Positioning

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import tcod
+from tcod import Console
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -13,12 +17,12 @@ def main() -> None:
             screen_width,
             screen_height,
             tileset=tileset,
-            title="Yet Another Roguelike Tutorial",
+            title="The Silence of the Lambs",
             vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="*")
 
             context.present(root_console)
 
