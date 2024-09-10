c90e7de5db668c9464f3a5e604a66e5042079bdd
obafisoye <dannieoba@gmail.com>
2024-09-10 12:54:08 -0500

    Updated

---
+++ entity.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+
+class Entity:
+    """
+    GENERIC OBJECT TO REPRESENT PLAYERS, ENEMIES, ITEMS etc.
+    """
+    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx: int, dy:int) -> None:
+        # MOVE THE ENTITY BY A GIVEN AMOUNT
+        self.x += dx
+        self.y += dy
+
